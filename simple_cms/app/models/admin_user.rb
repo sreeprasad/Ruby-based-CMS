505df8174b0e9176e4f1f9b4c57e1c3224ad1ae1
Sreeprasad <sreeprasad.sp@gmail.com>
2012-05-10 12:51:54 -0400

    adding scope

--- simple_cms/app/models/admin_user.rb
+++ simple_cms/app/models/admin_user.rb
@@ -1,3 +1,4 @@
 class AdminUser < ActiveRecord::Base
   # attr_accessible :title, :body
+  scope :named, lambda{|first,last| where (:first_name =>first, :last_name=>last)}
 end
--- simple_cms/app/models/subject.rb
+++ simple_cms/app/models/subject.rb
@@ -1,3 +1,6 @@
 class Subject < ActiveRecord::Base
   # attr_accessible :title, :body
+  scope :visible, where(:visible => true)
+  scope :invisible, where(:visible => false)
+  scope :search, lambda{|query| where (["name LIKE ?","%#{query}%"])}
 end
