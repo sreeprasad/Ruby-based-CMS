7c09414d35ce46ea3aa96950d632cb56b19719ef
Sreeprasad <sreeprasad.sp@gmail.com>
2012-05-31 11:52:11 -0400

    updated attibute

--- simple_cms/app/models/admin_user.rb
+++ simple_cms/app/models/admin_user.rb
@@ -1,4 +1,4 @@
 class AdminUser < ActiveRecord::Base
   # attr_accessible :title, :body
-  scope :named, lambda{|first,last| where (:first_name =>first, :last_name=>last)}
+  scope :named, lambda{|first,last| where (:first_name =>first, :middle_name=>middle, :last_name=>last)}
 end
